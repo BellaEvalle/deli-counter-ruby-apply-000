5462099856cd916c676079389ebca1cefd8f76d4
Bella Evalle <bellaevalle@yahoo.com>
2020-04-03 20:26:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,22 @@
 katz_deli = []
 
 def line(katz_deli)
-  line_positions = [];
+  the_line = [];
   if katz_deli.empty?
     puts "The line is currently empty."
   else
     number = 1
     katz_deli.each do |customer|
-      line_positions << "#{number}. #{customer}"
+      the_line << "#{number}. #{customer}"
       number += 1
     end
-    puts "The line is currently: #{line_positions.join(" ")}"
+    puts "The line is currently: #{the_line.join(" ")}"
   end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
